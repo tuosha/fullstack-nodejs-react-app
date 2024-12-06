a734c75899bd071e79c26cb7b29e8b341d20d3b0
tuosha <tuosha@yandex.ru>
2024-12-06 18:28:52 +0300

    add css modules

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,10 +1,11 @@
 import AppRouterProvider from './providers/RouterProvider'
+import classes from './App.module.css'
 
 const App = () => {
     return (
-        <>
+        <div className={classes.app}>
             <AppRouterProvider />
-        </>
+        </div>
     )
 }
 
