6f5edab669f181593f5b2f12d867210ff793a336
tuosha <tuosha@yandex.ru>
2025-01-08 11:27:44 +0300

    add css lint, add babel, add jest, add classNames.test.ts

---
+++ frontend/config/jest/jest.config.ts
@@ -0,0 +1,18 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest'
+
+const config: Config = {
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    moduleDirectories: ['node_modules'],
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+    rootDir: '../../',
+}
+
+export default config
