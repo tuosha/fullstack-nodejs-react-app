b506b4b34b3a805b4c2ce34f23f1e72ea7edd64f
tuosha <tuosha@yandex.ru>
2025-01-29 21:45:05 +0300

    add working loki tests, but have some errors on npm audit

--- frontend/config/storybook/preview.ts
+++ frontend/config/storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react'
 import { withGlobalImports } from '../../src/shared/config/storybooks/decorators/withGlobalmports'
-import 'loki/configure-react'
+import 'loki/configure-react.js'
 
 const preview: Preview = {
     decorators: [withGlobalImports],
