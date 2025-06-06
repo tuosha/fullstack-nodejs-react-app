cca18170809bc7bb05f3c58c4264567e9bdfe7e6
tuosha <tuosha@yandex.ru>
2025-06-06 21:39:10 +0300

    fix use effect deps & bundle analyzer

--- frontend/src/shared/lib/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ frontend/src/shared/lib/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -32,6 +32,7 @@ const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = (props) => {
                 })
             }
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return <>{children}</>
