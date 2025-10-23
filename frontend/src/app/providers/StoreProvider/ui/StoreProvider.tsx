4b739213231b7d0011e874ec05918c04d3158b7e
tuosha <tuosha@yandex.ru>
2025-10-23 23:07:52 +0300

    add shared api, add thunkExtra, fix navigation, fix StoreProvider

--- frontend/src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ frontend/src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -2,6 +2,8 @@ import { ReactNode } from 'react'
 import { Provider } from 'react-redux'
 import { createReduxStore } from '../config/store'
 import { StateScheme } from '../config/StateScheme'
+import { $api } from '../../../../shared/api/api'
+import { useNavigate } from 'react-router-dom'
 
 export interface StoreProviderProps {
     children?: ReactNode
@@ -9,7 +11,11 @@ export interface StoreProviderProps {
 }
 
 const StoreProvider = ({ children, initialState }: StoreProviderProps) => {
-    const store = createReduxStore(initialState)
+    const navigate = useNavigate()
+    const store = createReduxStore(initialState, {
+        api: $api,
+        navigate,
+    })
     return <Provider store={store}>{children}</Provider>
 }
 
--- frontend/src/features/AuthByUsername/services/loginByUsername.ts
+++ frontend/src/features/AuthByUsername/services/loginByUsername.ts
@@ -3,28 +3,34 @@ import axios, { AxiosError } from 'axios'
 import { userActions } from '../../../entites/User'
 import { USER_LOCAL_STORAGE_KEY } from '../../../shared/constants/localstorage'
 import { UserType } from '../../../entites/User'
+import { Extra } from '../../../app/providers/StoreProvider/config/store'
 
 interface LoginProps {
     username: string
     password: string
 }
 
-export const loginByUsername = createAsyncThunk<UserType, LoginProps, { rejectValue: Error | AxiosError }>(
-    'users/loginByUsername',
-    async (authData, thunkAPI) => {
-        try {
-            const response = await axios.post<UserType>('http://localhost:8000/login', authData)
-            if (!response.data) {
-                throw new Error()
-            }
-            localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(response.data))
-            thunkAPI.dispatch(userActions.setAuthData(response.data))
-            return response.data
-        } catch (error) {
-            if (axios.isAxiosError(error)) {
-                console.log(error)
-            }
-            return thunkAPI.rejectWithValue(error)
+export const loginByUsername = createAsyncThunk<
+    UserType,
+    LoginProps,
+    {
+        rejectValue: Error | AxiosError
+        extra: Extra
+    }
+>('users/loginByUsername', async (authData, { extra, dispatch, rejectWithValue }) => {
+    try {
+        const response = await extra.api.post('/login', authData)
+        if (!response.data) {
+            throw new Error()
         }
-    },
-)
+        localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(response.data))
+        dispatch(userActions.setAuthData(response.data))
+        extra.navigate('/profile-page')
+        return response.data
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log(error)
+        }
+        return rejectWithValue(error)
+    }
+})
